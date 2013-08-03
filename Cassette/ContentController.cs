88bb391ace99f749aee8e0da886f2b1a99bfc8e2
David Siegel <djsiegel@gmail.com>
2013-08-03 16:10:30 -0700

    Player mockup

--- Cassette/ContentController.cs
+++ Cassette/ContentController.cs
@@ -17,22 +17,33 @@ namespace Cassette
 		UIView OriginalView;
 		RectangleF OriginalFrame;
 		BigCoverView CoverView;
+		UIImageView ControlView;
 
 		public ContentController (RectangleF frame)
 		{
 			View = new UIView (frame);
+
+			CoverView = new BigCoverView ();
+			ControlView = new UIImageView (UIImage.FromFile ("player.png"));
 			View.AddSubview (CoverCollection = new CoverCollectionView (frame));
+
 			CoverCollection.CoverTapped += CoverCollectionCoverTapped;
+			CoverView.Tapped += _ => TransitionFromBigCoverBackToCoverCollection ();
 		}
 
 		void TransitionFromBigCoverBackToCoverCollection ()
 		{
-			UIView.Animate (0.2, () => {
+			UIView.Animate (0.1, () => {
+				ControlView.Alpha = 0;
+			}, () => {
+				ControlView.RemoveFromSuperview ();
+			});
+
+			UIView.Animate (0.2, 0.1, UIViewAnimationOptions.CurveEaseOut, () => {
 				CoverView.Frame = OriginalFrame;
 				CoverCollection.Alpha = 1;
 			}, () => {
 				CoverView.RemoveFromSuperview ();
-				CoverView = null;
 				OriginalView.Alpha = 1;
 			});
 		}
@@ -42,16 +53,23 @@ namespace Cassette
 			OriginalView = view;
 			OriginalFrame = CoverCollection.ConvertRectToView (view.Frame, View);
 
-			CoverView = new BigCoverView { Frame = OriginalFrame, Image = cover.CoverImage };
-			CoverView.Tapped += _ => TransitionFromBigCoverBackToCoverCollection ();
+			CoverView.Frame = OriginalFrame;
+			CoverView.Image = cover.CoverImage;
 		
 			View.AddSubview (CoverView);
 
 			OriginalView.Alpha = 0;
+
 			UIView.Animate (0.2, () => {
 				CoverCollection.Alpha = 0;
 				CoverView.Frame = new RectangleF (0, 0, View.Frame.Width, View.Frame.Width);
 			});
+
+			ControlView.Alpha = 0;
+			View.AddSubview (ControlView);
+			UIView.Animate (0.1, 0.25, UIViewAnimationOptions.CurveLinear, () => {
+				ControlView.Alpha = 1;
+			}, () => {});
 		}
 	}
 	
