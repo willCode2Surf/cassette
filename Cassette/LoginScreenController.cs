0d99ef9362c2eee6fa6dc2bfdbb31cb4b6eacbf1
Stuart Bray <stu.bray@gmail.com>
2013-08-11 15:58:48 -0700

    Refine RdioClient (previously RdioHelper)

--- Cassette/LoginScreenController.cs
+++ Cassette/LoginScreenController.cs
@@ -1,9 +1,7 @@
 using System;
-
 using System.Linq;
 using System.Drawing;
 using System.Collections.Generic;
-
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using MonoTouch.CoreGraphics;
@@ -14,63 +12,55 @@ namespace Cassette
 	{
 		UIButton loginButton;
 
-		public LoginScreenController ()
+		public LoginScreenController (RectangleF frame)
 		{
-			View = new UIView (UIScreen.MainScreen.Bounds);
+			View = new UIView (frame);
 			View.AddSubview (new UIImageView (UIImage.FromFile ("purple.png")));
 
-			float width = UIScreen.MainScreen.ApplicationFrame.Width;
+			float width = frame.Width;
 			float delta = 20f; 
-			float buttonWidth = width - ( 2 * delta );
+			float buttonWidth = width - (2 * delta);
 			float buttonHeigth = 2 * delta;
 
-			loginButton = UIButton.FromType(UIButtonType.RoundedRect);
-			loginButton.Frame = new RectangleF(delta, delta, buttonWidth, buttonHeigth);;
-			loginButton.SetTitle("Login to Rdio", UIControlState.Normal);
+			loginButton = UIButton.FromType (UIButtonType.RoundedRect);
+			loginButton.Frame = new RectangleF (delta, delta, buttonWidth, buttonHeigth);
+			loginButton.SetTitle ("Login to Rdio", UIControlState.Normal);
 
-			loginButton.SetTitleColor(UIColor.Black, UIControlState.Normal);
-			loginButton.TouchUpInside += (sender, e) => 
-			{
-				RdioHelper.Instance.AuthorizeFromController(this);
+			loginButton.SetTitleColor (UIColor.Black, UIControlState.Normal);
+			loginButton.TouchUpInside += (sender, e) => {
+				RdioClient.SharedClient.AuthorizeFromController (this);
 			};
 
-			RdioHelper.AuthorizeRequestCompleted += (RdioHelper.AuthorizeState state) => {
-				switch(state) 
-				{
-					case RdioHelper.AuthorizeState.Authorized:
-					{	
-						this.PresentingViewController.DismissViewController(false,null);
-						break;
-					}
-						case RdioHelper.AuthorizeState.AuthorizeFailure:
-					{
-						new UIAlertView("Authentication Failed", "Failed to login to Rdio" , null, "ok", null).Show(); 
-						this.View.AddSubview(loginButton);
-						break;
-					}
-						case RdioHelper.AuthorizeState.AuthorizeCancelled:
-					{
-						new UIAlertView("Authentication Cancelled", "You cancelled your authentication", null, "ok", null).Show(); 
-						this.PresentingViewController.DismissViewController(false,null);
-						break;
-					}
-					default:
-						break;
+			RdioClient.SharedClient.AuthorizeRequestCompleted += (state) => {
+				switch (state) {
+				case AuthorizeState.Authorized:
+					PresentingViewController.DismissViewController (false, null);
+					break;
+				case AuthorizeState.Failed:
+					new UIAlertView ("Authentication Failed", "Failed to login to Rdio", null, "ok", null).Show (); 
+					this.View.AddSubview (loginButton);
+					break;
+				case AuthorizeState.Cancelled:
+					new UIAlertView ("Authentication Cancelled", "You cancelled your authentication", null, "ok", null).Show (); 
+					PresentingViewController.DismissViewController (false, null);
+					break;
+				default:
+					break;
 				}
 			};
 		}
 
 		public override void ViewDidAppear (bool animated)
 		{
-			if (RdioHelper.Instance.IsAuthorized) {
+			if (RdioClient.SharedClient.IsAuthorized) {
 				// After the user authenticates with the Rdio login screen we're done
-				this.PresentingViewController.DismissViewController(false,null);
-			} else if (!string.IsNullOrEmpty (RdioHelper.Instance.AuthToken)) {
+				PresentingViewController.DismissViewController (false, null);
+			} else if (!string.IsNullOrEmpty (RdioClient.SharedClient.AuthToken)) {
 				// On first launch if the user has previously authenticated
-				RdioHelper.Instance.AuthorizeWithToken (RdioHelper.Instance.AuthToken);
+				RdioClient.SharedClient.AuthorizeWithToken (RdioClient.SharedClient.AuthToken);
 			} else {
 				// On first launch with no authentication stored 
-				this.View.AddSubview(loginButton);
+				View.AddSubview (loginButton);
 			}
 		}
 	}
