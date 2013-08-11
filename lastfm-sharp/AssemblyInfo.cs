2a951e73b4c029aab9640056724afdf8c1c19fed
David Siegel <djsiegel@gmail.com>
2013-08-11 15:30:15 -0700

    Disable signing

--- lastfm-sharp/AssemblyInfo.cs
+++ lastfm-sharp/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.CompilerServices;
 // if desired. See the Mono documentation for more information about signing.
 
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("../lastfm-sharp.snk")]
+//[assembly: AssemblyKeyFile("../lastfm-sharp.snk")]
